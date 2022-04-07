ef351264adc65e04e419e80743b93662b8c40176
prabhatM102 <prabhat@rudrainnovative.in>
2022-04-07 09:55:28 +0530

    For deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "dist"));
+app.use(express.static(__dirname + "/dist/form-handling"));
 
 app.all("*", (req, res) => {
   res.status(200).sendFile(__dirname + "/dist/form-handling/index.html");
